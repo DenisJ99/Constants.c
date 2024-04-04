c8f80e6ceaeaa3027ab8e9d250d4f07b9696fbf4
Denis Johnson <166014361+DenisJ99@users.noreply.github.com>
2024-04-04 14:51:56 +0100

    Add files via upload

---
+++ Constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    const float PI = 3.14159;
+
+    printf("%f", PI);
+
+    return 0;
+}
+/*Constant = Fixed value that cannot be altered by the program 
+during its execution.
+*/
